e95f5481d434e86f9411d350860ba7d94c6e60c4
Qubo Qin <qubo.qin.2018@gmail.com>
2018-08-25 13:44:31 -0700

    create a new project based on the Tabbed App template for MVC paradigm

---
+++ CryptoMarketIOS/CryptoMarketMVC/CryptoMarketMVC/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CryptoMarketMVC
+//
+//  Created by Qubo on 8/25/18.
+//  Copyright © 2018 Qubo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CryptoMarketIOS/CryptoMarketMVC/CryptoMarketMVC/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CryptoMarketMVC
+//
+//  Created by Qubo on 8/25/18.
+//  Copyright © 2018 Qubo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
